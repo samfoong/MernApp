96be184d2aeda7206ae27aae2b576e34f56a0dfe
samfoong <samfoong88@gmail.com>
2018-07-18 14:51:38 +1000

    Connect To MongoDB with mongoose and add gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected')) // if connected successfully
+    .catch(err => console.log(err)); //if error
 
 app.get('/', (req,res) => res.send('Hello World'));
 // '/' is the homepage, send a request when we are at the homepage and respond by sending Hello
